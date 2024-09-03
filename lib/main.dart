bd5a7b33e326f2a496075c1fefb0773e094ad06b
Muneeb-Usman~ <muneebusman54@gmail.com~>
2024-09-03 15:33:59 +0500

    Created screen two and fetched image data

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:postman_practice/example_two.dart';
 import 'package:postman_practice/home_screen.dart';
 
 void main() {
@@ -18,7 +19,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: HomeScreen(),
+      home: ExampleTwo(),
     );
   }
 }
